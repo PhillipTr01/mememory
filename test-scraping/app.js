b0ab85ff79318efe1728a155e24103019a6156f8
Phillip Tran <inf19105@lehre.dhbw-stuttgart.de>
2021-03-14 19:16:59 +0100

    Added test-scraping

---
+++ test-scraping/app.js
@@ -0,0 +1,20 @@
+var request = require('request');
+
+request('https://www.reddit.com/r/dankmemes/.json?limit=100', function(err, res, body) {
+
+
+    if(!err && res.statusCode === 200) {
+        var $ = JSON.parse(body);
+
+        var count = 0;
+
+        for(image of $.data.children) {
+            if(count != 0) {
+                console.log(image.data.url_overridden_by_dest);
+            }
+            count++;
+        }
+    } else {
+        console.log(err + " " + res.statusMessage + " " + res.statusCode);
+    }
+});
