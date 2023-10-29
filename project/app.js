5a8851e7ac3a87bec8a4d247138e4ceaa00b5aaa
unknown <Phillip-Thanh-Vu.Tran@t-systems.com>
2023-10-29 18:40:51 +0100

    Replaced request with got

--- project/app.js
+++ project/app.js
@@ -102,7 +102,7 @@ mongoose.connect(process.env.DB_URL, {
 });
 
 /* Get new Memes every 10 minutes */
-setInterval(() => memeScraper.scrape(), 600000);
+setInterval(() => memeScraper.scrape(), 10 * 60 * 1000);
 
 memeScraper.scrape();
 
--- project/meme_scraper.js
+++ project/meme_scraper.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var got = require('got');
 const Meme = require('./models/Meme');
 
 module.exports.scrape = function () {
@@ -8,25 +8,18 @@ module.exports.scrape = function () {
 };
 
 function scrapeImages(link) {
-    request(link, async function (err, res, body) {
-        if (!err && res.statusCode === 200) {
-            try {
-                var parsedBody = JSON.parse(body);
-                var skipFirst = false;
+    got(link).then(async (response) => {
+        var parsedBody = JSON.parse(response.body);
 
-                for (image of parsedBody.data.children) {
-                    if (skipFirst && /https\:\/\/i\.redd\.it\/([0-9A-Za-z]{13})(.jpg|.png|.gif)/.test(image.data.url_overridden_by_dest)) {
-                        await Meme({
-                            link: image.data.url_overridden_by_dest
-                        }).save();
-                    }
-                    skipFirst = true;
-                }
-            } catch (error) {
-                console.log(error);
+        for (image of parsedBody.data.children.slice(1)) {
+            if (/https\:\/\/i\.redd\.it\/([0-9A-Za-z]{13})(.jpg|.png|.gif)/.test(image.data.url_overridden_by_dest)) {
+                console.log(image.data.url_overridden_by_dest)
+                await Meme({
+                    link: image.data.url_overridden_by_dest
+                }).save();
             }
-        } else {
-            console.log(err + " " + res.statusMessage + " " + res.statusCode);
         }
-    });
+      }).catch(error => {
+        console.log(error);
+      });
 }
